e608b73150f06d96165a063c5a81414fe2ca3ab0
duonganh203 <rankill52@gmail.com>
2024-04-04 16:47:15 +0700

    adding a new page to the marketing site

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div>
+            <Navbar />
+            <main>{children}</main>
+            {/* <Footer/> */}
+        </div>
+    );
+};
+
+export default MarketingLayout;
